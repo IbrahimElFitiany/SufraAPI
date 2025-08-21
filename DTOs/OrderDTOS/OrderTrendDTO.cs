fa4237a51084567db9f26b24081b7dcdc9109242
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-08-21 03:42:23 +0300

    feat(order): add order trend feature with current/previous counts

---
+++ DTOs/OrderDTOS/OrderTrendDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Sufra.DTOs.OrderDTOS
+{
+    public class OrderTrendDTO
+    {
+        public int Current { get; set; }
+        public int Previous { get; set; }
+        public int Diff { get; set; }
+    }
+}
